6099b79599f44a599f68b79fc83d5cbd9eba07a2
Haruna Gana <62021895+HarryGana@users.noreply.github.com>
2020-04-22 23:11:16 +0100

    Initial commit

---
+++ Python Guessing game Task .py
@@ -0,0 +1,76 @@
+import random
+print("Welcome to Start.NG number guess game,there are three levels in the game: easy, medium and hard ")
+user_name = input('Please enter your user name to begin: ').upper()
+user_level = input('Please pick your preferred level: ').lower()
+Turns =0
+limit = 6
+left = 6
+while user_level == 'easy' :
+    random_number = random.randint(1, 10)
+    try:
+        Your_guess = input("Enter a number between 1 and 10: ")
+        Turns += 1
+        left -= 1
+        print(f"you have {left} trial(s) left ")
+        if Turns == limit:
+            print(f'Game 0ver! {user_name}')
+            break
+        if random_number == int(Your_guess):
+            print("You got it right!")
+            print("Number of trials you have used: ", Turns)
+            break
+        else:
+            if random_number > int(Your_guess):
+                print("that was wrong")
+            else:
+                print("that was wrong")
+    except ValueError:
+        print("only integers are allowed")
+Turns =0
+limit = 4
+left = 4
+while user_level == 'medium' :
+    random_number = random.randint(1, 20)
+    try:
+        Your_guess = input("Enter a number between 1 and 20: ")
+        Turns += 1
+        left -= 1
+        print(f"{user_name} have {left} trial(s) left ")
+        if Turns == limit:
+            print(f'Game Over {user_name}')
+            break
+        if random_number == int(Your_guess):
+            print(f"You got it right! {user_name} ")
+            print("Number of trials you have used: ", Turns)
+            break
+        else:
+            if random_number > int(Your_guess):
+                print("that was wrong")
+            else:
+                print("that was wrong")
+    except ValueError:
+        print("only integers are allowed")
+Turns =0
+limit = 3
+left = 3
+while user_level == 'hard' :
+    random_number = random.randint(1, 50)
+    try:
+        Your_guess = input("Enter a number between 1 and 50: ")
+        Turns += 1
+        left -= 1
+        print(f"you have {left} trial(s) left ")
+        if Turns == limit:
+            print(f'Game Over {user_name}')
+            break
+        if random_number == int(Your_guess):
+            print("You got it right!")
+            print("Number of trials you have used: ", Turns)
+            break
+        else:
+            if random_number > int(Your_guess):
+                print("Number is out of range")
+            else:
+                print("That was wrong")
+    except ValueError:
+        print("only integers are allowed")
